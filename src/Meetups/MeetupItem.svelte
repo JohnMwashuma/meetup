55e1edc7ab5c95e05b9aded847a69d43542b0433
John Mwashuma <jmwashuma@live.com>
2020-08-20 16:51:35 +0300

    Update Buttons text

--- src/Meetups/MeetupItem.svelte
+++ src/Meetups/MeetupItem.svelte
@@ -77,7 +77,7 @@
   <header>
     <h1>
       {title}
-      {#if isFav }
+      {#if isFav}
         <Badge>FAVORITE</Badge>
       {/if}
     </h1>
@@ -91,13 +91,14 @@
     <p>{description}</p>
   </div>
   <footer>
-    <Button href="mailto:{email}" caption="Contact" />
+    <Button href="mailto:{email}">Contact</Button>
     <Button
       mode="outline"
       color={isFav ? null : 'success'}
       type="button"
-      caption={isFav ? 'Unfavourite' : 'Favourite'}
-      on:click={() => dispatch('togglefavourite', id)} />
-    <Button type="button" caption="Show Details" />
+      on:click={() => dispatch('togglefavourite', id)}>
+      {isFav ? 'Unfavourite' : 'Favourite'}
+    </Button>
+    <Button type="button">Show Details</Button>
   </footer>
 </article>
